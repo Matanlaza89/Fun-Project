137fed9a86c6e58641cb3b72355a95afdd022bb7
Matan Leizerovich <matanlaza@gmail.com>
2022-01-01 18:08:59 +0200

    Update funProjectTB.vhd

--- funProjectTB.vhd
+++ funProjectTB.vhd
@@ -3,19 +3,18 @@
 -----------------------------------------------------------------------------
 
 -----------------------------------------------------------------------------
--------			           Fun Project TestBench			   	      -------
+-------			    Fun Project TestBench	  	      -------
 -----------------------------------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
 
 entity funProjectTB is
 end entity funProjectTB;
 
 architecture sim of funProjectTB is
 	-- Constants --
-	constant C_PERIOD : time := 20 ns; -- T = 1/f => T=1/50*10^6 => T = 20nsec
+	constant c_PERIOD : time := 20 ns; -- T = 1/f => T=1/50*10^6 => T = 20nsec
 	
 	-- Stimulus signals --
 	signal i_clk   : std_logic;
@@ -42,14 +41,15 @@ begin
 			HEX0  => o_HEX0 ,
 			HEX1  => o_HEX1 , 
 			HEX2  => o_HEX2 ,
-			HEX3  => o_HEX3);
+			HEX3  => o_HEX3
+		 );
 			
 
 	-- Testbench process --
 	p_TB : process
 	begin
-		i_reset <= '0'; wait for C_PERIOD;
-		i_reset <= '1'; wait for C_PERIOD * 15; 
+		i_reset <= '0'; wait for c_PERIOD;
+		i_reset <= '1'; wait for c_PERIOD * 15; 
 		wait;
 	end process p_TB;
 	
@@ -57,8 +57,8 @@ begin
 	-- 50 MHz clock in duty cycle of 50% - 20 ns
 	p_clock : process 
 	begin 
-		i_clk <= '0'; wait for C_PERIOD/2; -- 10 ns
-		i_clk <= '1'; wait for C_PERIOD/2; -- 10 ns
+		i_clk <= '0'; wait for c_PERIOD/2; -- 10 ns
+		i_clk <= '1'; wait for c_PERIOD/2; -- 10 ns
 	end process p_clock;
 
 end architecture sim;
